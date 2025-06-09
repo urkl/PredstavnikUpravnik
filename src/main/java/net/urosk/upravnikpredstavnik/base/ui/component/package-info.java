5d58b172d4b0506c29e19e68b307e3c708618770
start.vaadin.com <no-reply@vaadin.com>
2025-06-09 04:30:05 +0000

    Generated project

---
+++ src/main/java/net/urosk/upravnikpredstavnik/base/ui/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable UI components.
+ */
+@NullMarked
+package net.urosk.upravnikpredstavnik.base.ui.component;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/net/urosk/upravnikpredstavnik/base/ui/view/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable or cross-cutting view-related classes.
+ */
+@NullMarked
+package net.urosk.upravnikpredstavnik.base.ui.view;
+
+import org.jspecify.annotations.NullMarked;
