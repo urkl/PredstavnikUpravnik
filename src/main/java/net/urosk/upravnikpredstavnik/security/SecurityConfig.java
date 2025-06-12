a47849e167e6257c144819604b58b8dc10053ee5
Uroš Kristan <uros.kristan@gmail.com>
2025-06-12 08:47:37 +0200

    erger

--- src/main/java/net/urosk/upravnikpredstavnik/security/SecurityConfig.java
+++ src/main/java/net/urosk/upravnikpredstavnik/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @EnableWebSecurity
@@ -23,14 +24,16 @@ public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        super.configure(http);
         // Najprej dovolimo dostop do javnih virov
         http.authorizeHttpRequests(auth -> auth.requestMatchers("/images/**", "/themes/**").permitAll());
+        http.authorizeHttpRequests(
+                authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/images/*.png")).permitAll());
 
-        // --- SPREMEMBA: Klic super.configure() prestavimo na začetek ---
-        // To nastavi Vaadinove privzete varnostne nastavitve, ki jih nato povozimo.
+        // Icons from the line-awesome addon
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(new AntPathRequestMatcher("/line-awesome/**/*.svg")).permitAll());
 
+        super.configure(http);
         // ----------------------------------------------------------------
 
         // Sedaj definiramo naše lastne nastavitve, ki bodo imele prednost
